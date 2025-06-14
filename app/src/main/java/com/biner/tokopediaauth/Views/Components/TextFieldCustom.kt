afd032dce86e616752db40630c1d0eb1a955f5a3
Rangga Biner <rangga@ranggabiner.com>
2025-06-15 05:14:03 +0700

    add: verify page

--- app/src/main/java/com/biner/tokopediaauth/Views/Components/TextFieldCustom.kt
+++ app/src/main/java/com/biner/tokopediaauth/Views/Components/TextFieldCustom.kt
@@ -2,6 +2,7 @@ package com.biner.tokopediaauth
 
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.filled.Visibility
 import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.material3.Icon
@@ -12,6 +13,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
@@ -30,6 +32,7 @@ fun TextFieldCustom(label: String, password: Boolean, value: String) {
             .fillMaxWidth(),
         shape = RoundedCornerShape(16.dp),
         singleLine = true,
+        keyboardOptions = if (password && !passwordVisible.value) KeyboardOptions(keyboardType = KeyboardType.Password) else KeyboardOptions(keyboardType = KeyboardType.Email),
         visualTransformation = if (password && !passwordVisible.value) PasswordVisualTransformation() else VisualTransformation.None,
         trailingIcon = {
             if (password) {
--- app/src/main/java/com/biner/tokopediaauth/Views/VerifyView.kt
+++ app/src/main/java/com/biner/tokopediaauth/Views/VerifyView.kt
@@ -2,31 +2,40 @@ package com.biner.tokopediaauth
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Arrangement.spacedBy
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.outlined.Email
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
+
 @Composable
 fun VerifyView(email: String) {
+    var code by remember { mutableStateOf("") }
+
     Column(
         modifier = Modifier
             .padding(vertical = 16.dp)
@@ -34,39 +43,56 @@ fun VerifyView(email: String) {
             .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Icon(
-            imageVector = Icons.Outlined.Email,
-            modifier = Modifier.size(23.dp),
-            tint = MaterialTheme.colorScheme.primary,
-            contentDescription = "Email Icon",
-        )
         Column(
-            verticalArrangement = Arrangement.spacedBy(2.dp),
+            verticalArrangement = spacedBy(7.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(
-                "Masukkan Kode Verifikasi",
-                style = MaterialTheme.typography.titleLarge,
-                fontWeight = FontWeight.Bold,
-                textAlign = TextAlign.Center
+            Icon(
+                imageVector = Icons.Outlined.Email,
+                modifier = Modifier.size(30.dp),
+                tint = MaterialTheme.colorScheme.primary,
+                contentDescription = "Email Icon",
             )
-            Text(
-                "Kode verifikasi telah dikirim melalui e-mail ke ${email}",
-                style = MaterialTheme.typography.bodyMedium,
-                textAlign = TextAlign.Center
-            )
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    "Masukkan Kode Verifikasi",
+                    style = MaterialTheme.typography.titleLarge,
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center
+                )
+                Text(
+                    "Kode verifikasi telah dikirim melalui e-mail ke ${email}",
+                    style = MaterialTheme.typography.bodyMedium,
+                    textAlign = TextAlign.Center
+                )
+            }
         }
         Column(
             verticalArrangement = Arrangement.spacedBy(12.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
             ) {
             TextField(
-                value = "",
-                onValueChange = {},
-                label = { Text("Kode Verifikasi") },
-                placeholder = { Text("Masukkan kode verifikasi") },
+                value = code,
+                onValueChange = { input ->
+                    val filtered = input.filter { it.isDigit() }.take(6)
+                    code = filtered
+                },
+                modifier = Modifier.width(200.dp),
                 singleLine = true,
                 maxLines = 1,
                 isError = false,
+                textStyle = MaterialTheme.typography.displayMedium.copy(fontWeight = FontWeight.Bold, textAlign =  TextAlign.Center),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                colors = TextFieldDefaults.colors(
+                    focusedContainerColor = Color.Transparent,
+                    unfocusedContainerColor = Color.Transparent,
+                    disabledContainerColor = Color.Transparent,
+                    focusedIndicatorColor = MaterialTheme.colorScheme.primary,
+                    unfocusedIndicatorColor = Color.Gray,
+                    disabledIndicatorColor = Color.LightGray
+                )
             )
             Row {
                 Text(
